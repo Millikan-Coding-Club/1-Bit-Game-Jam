9224a677be7674af501dd5075fd7bbd506f2f2e8
YeetmyFeet <yeetmyfeet69420bruh@gmail.com>
2023-11-12 23:26:09 -0800

    Added asteroids
    -gave each asteroid a random sprite and spin speed
    -added new base animation

--- Earth Defender/Assets/Scripts/Asteroids.cs
+++ Earth Defender/Assets/Scripts/Asteroids.cs
@@ -1,22 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 using UnityEngine;
 
-public class Missile : MonoBehaviour
+public class Asteroids : MonoBehaviour
 {
     [SerializeField] float speed = 1f;
-    public GameObject missilePrefab;
+    private float rotationSpeed;
+    public float maxSpin = 80f;
     // Start is called before the first frame update
     void Start()
     {
-
+        rotationSpeed = Random.Range(maxSpin * -1, maxSpin);
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, new Vector2(0, 0), speed * Time.deltaTime);
-        transform.up = new Vector3(0f, 0f) - transform.position;
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0, 0, rotationSpeed * Time.deltaTime));
     }
 }
--- Earth Defender/Assets/Scripts/GameController.cs
+++ Earth Defender/Assets/Scripts/GameController.cs
@@ -3,21 +3,38 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.U2D;
 using UnityEngine.UIElements;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject missilePrefab;
-    public GameObject baseObject;
+    public GameObject asteroidPrefab;
     public GameObject satellite;
+    [SerializeField] private SpriteAtlas atlas;
     public int baseAmount = 5;
     public float distance = 10;
-    private float earthRadius = 5;
+    string[] asteroids =
+    {
+        "ED_asteroids1_0",
+        "ED_asteroids1_1", 
+        "ED_asteroids1_2",
+        "ED_asteroids1_3",
+        "ED_asteroids1_4",
+        "ED_asteroids1_5",
+        "ED_asteroids1_6",
+        "ED_asteroids1_7",
+        "ED_asteroids1_8",
+        "ED_asteroids1_9",
+        "ED_asteroids1_10",
+        "ED_asteroids1_11",
+        "ED_asteroids1_12",
+        "ED_asteroids1_13",
+    };
     [SerializeField] private float interval = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("spawnMissile", 0, interval);
+        InvokeRepeating("spawnAsteroid", 0, interval);
     }
 
     // Update is called once per frame
@@ -26,11 +43,12 @@ public class GameController : MonoBehaviour
         
     }
 
-    private void spawnMissile()
+    private void spawnAsteroid()
     {
         float angle = Random.Range(0f, 2 * Mathf.PI);
-        float x = Mathf.Cos(angle) * distance * 2;
+        float x = Mathf.Cos(angle) * distance * 1.75f;
         float y = Mathf.Sin(angle) * distance;
-        Instantiate(missilePrefab, new Vector3(x, y, 0), transform.rotation);
+        asteroidPrefab.GetComponent<SpriteRenderer>().sprite = atlas.GetSprite(asteroids[Random.Range(0, 13)]);
+        Instantiate(asteroidPrefab, new Vector3(x, y, 0), transform.rotation);
     }
 }
