225fc233cbe8f29136fe5ef5b7a942a0de4435c1
YeetmyFeet <yeetmyfeet69420bruh@gmail.com>
2023-11-12 19:20:46 -0800

    Added stuff
    -added sprites
    -did other stuff as well

--- Earth Defender/Assets/Scripts/GameController.cs
+++ Earth Defender/Assets/Scripts/GameController.cs
@@ -9,7 +9,7 @@ public class GameController : MonoBehaviour
 {
     public GameObject missilePrefab;
     public GameObject baseObject;
-    public GameObject earth;
+    public GameObject satellite;
     public int baseAmount = 5;
     public float distance = 10;
     private float earthRadius = 5;
@@ -17,7 +17,6 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        spawnBases();
         InvokeRepeating("spawnMissile", 0, interval);
     }
 
@@ -33,18 +32,5 @@ public class GameController : MonoBehaviour
         float x = Mathf.Cos(angle) * distance * 2;
         float y = Mathf.Sin(angle) * distance;
         Instantiate(missilePrefab, new Vector3(x, y, 0), transform.rotation);
-    }
-
-    private void spawnBases()
-    {
-        earthRadius = (float)(earth.GetComponent<CircleCollider2D>().radius);
-        for (int i = 0; i < baseAmount; i++)
-        {
-            float angle = Random.Range(0f, 2 * Mathf.PI);
-            float x = Mathf.Cos(angle) * earthRadius;
-            float y = Mathf.Sin(angle) * earthRadius;
-            Instantiate(baseObject, new Vector3(x, y, 0), transform.rotation);
-        }
-
     }
 }
