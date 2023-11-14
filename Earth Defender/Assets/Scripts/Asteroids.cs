c035508df075ac92ecc9ab2caac7d0c16a9b62f1
YeetmyFeet <yeetmyfeet69420bruh@gmail.com>
2023-11-13 19:02:58 -0800

    Implemented clicking
    -added code to let the player click on the 2 bases and the satellite, as well as setting the target for the missile
    -set asteroid to a random number
    -set asteroid target to a random spot on earth so they don't all go towards the center

--- Earth Defender/Assets/Scripts/Asteroids.cs
+++ Earth Defender/Assets/Scripts/Asteroids.cs
@@ -2,23 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Asteroids : MonoBehaviour
 {
-    [SerializeField] float speed = 1f;
+    [SerializeField] private float minSpeed = 0.01f;
+    [SerializeField] private float maxSpeed = 3f;
+    [SerializeField] private float maxSpin = 80f;
+    private float speed;
     private float rotationSpeed;
-    public float maxSpin = 80f;
+    private float targetX;
+    private float targetY;
+
     // Start is called before the first frame update
     void Start()
     {
         rotationSpeed = Random.Range(maxSpin * -1, maxSpin);
+        speed = Random.Range(minSpeed, maxSpeed);
+        targetX = Random.Range(-1.4f, 1.4f);
+        targetY = Random.Range(-1.4f, 1.4f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, new Vector2(0, 0), speed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(targetX, targetY), speed * Time.deltaTime);
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0, 0, rotationSpeed * Time.deltaTime));
     }
 }
--- Earth Defender/Assets/Scripts/GameController.cs
+++ Earth Defender/Assets/Scripts/GameController.cs
@@ -9,7 +9,13 @@ using UnityEngine.UIElements;
 public class GameController : MonoBehaviour
 {
     public GameObject asteroidPrefab;
-    public GameObject satellite;
+    public GameObject crosshair;
+    public GameObject leftBase;
+    public GameObject rightBase;
+    public GameObject leftBaseSquare;
+    public GameObject rightBaseSquare;
+    public GameObject satelliteSquare;
+    public GameObject nuke;
     [SerializeField] private SpriteAtlas atlas;
     public int baseAmount = 5;
     public float distance = 10;
@@ -40,7 +46,38 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
+            if (hit.collider == null)
+            {
+                fireMissile();
+            } else
+            {
+                if (hit.collider.transform == satelliteSquare.transform)
+                {
+                    Debug.Log("satellite");
+                }
+
+                if (hit.collider.transform == leftBaseSquare.transform)
+                {
+                    Debug.Log("left base");
+                }
+
+                if (hit.collider.transform == rightBaseSquare.transform)
+                {
+                    Debug.Log("right base");
+                }
+            }
+        }
+    }
+
+    private void fireMissile()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Instantiate(crosshair, mousePos, transform.rotation);
+        Instantiate(nuke, rightBase.transform.position, Quaternion.Euler(rightBase.transform.rotation.eulerAngles - new Vector3(0, 0, 90)));
     }
 
     private void spawnAsteroid()
@@ -48,6 +85,7 @@ public class GameController : MonoBehaviour
         float angle = Random.Range(0f, 2 * Mathf.PI);
         float x = Mathf.Cos(angle) * distance * 1.75f;
         float y = Mathf.Sin(angle) * distance;
+        // give asteroid prefab a random asteroid sprite
         asteroidPrefab.GetComponent<SpriteRenderer>().sprite = atlas.GetSprite(asteroids[Random.Range(0, 13)]);
         Instantiate(asteroidPrefab, new Vector3(x, y, 0), transform.rotation);
     }
